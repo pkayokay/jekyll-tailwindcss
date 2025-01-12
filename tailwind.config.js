7a14da812afe3779483b1775020b6a2532c2b016
Paul Kim <paul@paulkim.net>
2025-01-11 21:18:12 -0500

    Set up tailwind (#2)

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  corePlugins: {
+    preflight: false,
+  },
+  content: [
+    "./_drafts/**/*.md",
+    "./_includes/**/*.html",
+    "./_layouts/**/*.html",
+    "./_pages/*.{html,md}",
+    "./_posts/*.md",
+    "./*.{html,md,markdown}",
+  ],
+};
