7290e186566a256e74b55b84ba680260bfc34fd4
Paul Kim <paul@paulkim.net>
2025-10-17 10:33:03 -0400

    Fix 'invalid byte sequence in US-ASCII' error and set encoding to UTF-8

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 export JEKYLL_ENV=production
+export LC_ALL="C.UTF-8"
+export LANG="en_US.UTF-8"
+export LANGUAGE="en_US.UTF-8"
 
 echo "Starting Jekyll build..."
 bundle exec jekyll build
