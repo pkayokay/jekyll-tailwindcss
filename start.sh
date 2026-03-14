5edb0db2f36bdff7bded6d8435c559e6721b4cfd
Paul Kim <paul@paulkim.net>
2026-03-14 12:48:49 -0400

    Run esbuild on start and watch for changes

--- start.sh
+++ start.sh
@@ -2,5 +2,14 @@
 
 PORT=${1:-4000}
 
+echo "Building JS..."
+yarn build:js
+
+echo "Starting JS watcher..."
+yarn watch:js &
+ESBUILD_PID=$!
+
+trap "kill $ESBUILD_PID" EXIT
+
 echo "Starting Jekyll development server with live reload on port $PORT..."
 bundle exec jekyll serve --livereload --port $PORT
